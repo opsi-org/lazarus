0f6455560942d62d8ab46183ebcaccaf2ec9484d
Detlef Oertel <d.oertel@uib.de>
2017-01-19 15:57:53 +0100

    lessons nofier3

---
+++ lessons/notifier/notifierform2a.pas
@@ -0,0 +1,31 @@
+unit notifierform2a;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ lessons/notifier/notifierform3a.pas
@@ -0,0 +1,34 @@
+unit notifierform3a;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  windows,
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Image1: TImage;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm2 }
+
+
+end.
+
