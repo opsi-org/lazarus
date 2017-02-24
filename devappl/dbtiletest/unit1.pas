17b0cf8c15e7352bcaf73d054861161e16dac4f9
Detlef Oertel <d.oertel@uib.de>
2017-02-24 18:18:46 +0100

    dbtiletest

--- devappl/dbtiletest/unit1.pas
+++ devappl/dbtiletest/unit1.pas
@@ -10,16 +10,36 @@ uses
 
 type
 
+  TPPanel = ^TPanel;
+  TPLabel = ^TLabel;
+
+
+  (*
+  TProductPanel = record
+    id : TLabel;
+    name : TLabel;
+    binstall : Tbutton;
+  end;
+  *)
+
+  TProductPanel = class(TPanel)
+    constructor create(id : string);
+  end;
+
   { TForm1 }
 
   TForm1 = class(TForm)
     FlowPanel1: TFlowPanel;
-    procedure FormCreate(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    function create_sub_panel :  TPPanel;
+    procedure create_sub_label(myp :  TPPanel);
   private
     { private declarations }
   public
     { public declarations }
   end;
+
+
 
 var
   Form1: TForm1;
@@ -30,14 +50,65 @@ implementation
 
 var
   myPanelList : TList;
+  ppanel:TPPanel;
+  plabel: TPLabel;
+  labl : TLabel;
+
+  constructor TProductPanel.create(id: string);
+begin
+  Inherited;
+end;
 
 { TForm1 }
 
-procedure TForm1.FormCreate(Sender: TObject);
-var
-  mypanel1,mypanel2 : TPanel;
-  mylabel1, mylabel2 :TLabel;
+procedure TForm1.create_sub_label(myp :  TPPanel);
+
 begin
+  new(plabel);
+  plabel^.Parent:= myp^;
+  plabel^.Caption:='huhu';
+  //repaint;
+end;
+
+
+function TForm1.create_sub_panel :  TPPanel;
+
+begin
+  new(result);
+  result^ := TPanel.Create(FlowPanel1);
+  result^.Parent:= FlowPanel1;
+  result^.Width:=100;
+  result^.Height:=100;
+  (*
+  new(plabel);
+  plabel^.Parent:= result^;
+  plabel^.Caption:='huhu';
+  *)
+  //repaint;
+end;
+
+
+procedure TForm1.FormActivate(Sender: TObject);
+var
+  mypanel1,mypanel2, panel : TPanel;
+  mylabel1, mylabel2 :TLabel;
+  myList : TList;
+begin
+  myList := TList.Create;
+  myList.Add(create_sub_panel);
+  create_sub_label(myList[0]);
+  myList.Add(create_sub_panel);
+  repaint;
+
+
+
+  (*
+  new(ppanel);
+  ppanel^ := TPanel.Create(FlowPanel1);
+  ppanel^.Parent:= FlowPanel1;
+  ppanel^.
+  *)
+  (*
   mypanel1 := TPanel.Create(FlowPanel1);
   mypanel1.Parent:= FlowPanel1;
   mypanel1.Width:=100;
@@ -55,6 +126,7 @@ begin
   mypanel2.Width:=100;
   mypanel2.Height:=100;
   mypanel2.Repaint;
+  *)
 end;
 
 end.
