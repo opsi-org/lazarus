a2be4e734369b975d135a907bb124f85a22171e2
Detlef Oertel <d.oertel@uib.de>
2018-09-25 14:54:53 +0200

    opsi_client_systray  4.1.0.1
      * switch to lazarus 1.8.4
      * fix logging to c:\opsi.org\applog
    
    -- Detlef Oertel <d.oertel@uib.de>  Tue, 25 Sep 2018:15:00:00 +0200

--- common/oslog.pas
+++ common/oslog.pas
@@ -1759,6 +1759,10 @@ begin
   StandardMainLogPath := StandardLogPath;
   StandardPartLogPath := 'c:\opsi.org\log\';
   {$IFDEF OPSI_AS_USER}
+  StandardLogPath := 'c:\opsi.org\applog\';
+  StandardMainLogPath := StandardLogPath;
+  StandardPartLogPath := StandardLogPath;
+  (*
   AppDataPath:='';
   SHGetFolderPath(0,CSIDL_APPDATA,0,SHGFP_TYPE_CURRENT,AppDataPath);
   //SHGetSpecialFolderPath(0,AppDataPath,CSIDL_LOCAL_APPDATA,false)
@@ -1768,6 +1772,7 @@ begin
     StandardMainLogPath := StandardLogPath;
     StandardPartLogPath := StandardLogPath;
   end;
+  *)
   {$ENDIF OPSI_AS_USER}
   {$ELSE OPSI}
   StandardLogPath := GetTempDir(false);
