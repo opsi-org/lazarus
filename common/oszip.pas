bdf01a7e2e307456569cab636195ef7988fddf88
Jeena <john@pcjeena.uib.local>
2019-03-21 18:18:52 +0100

    Add new Zipper function(Only works for folders, not for single file)

--- common/oszip.pas
+++ common/oszip.pas
@@ -8,11 +8,53 @@ interface
 uses
   Classes, SysUtils, Zipper;
 
+// Compress a folder which contains subfolders and files.
+function ZipWithDirStruct(File2Zip, OutputDir : String): Boolean;
+
 // Decompress a zip file while preserving its directory structure.
 function UnzipWithDirStruct(File2Unzip, OutputDir :String): Boolean;
 
 
 implementation
+
+function ZipWithDirStruct(File2Zip, OutputDir : String): Boolean;
+var
+  ZipperObj       : TZipper;
+  filecounter     : Integer;
+  FileList        : TStringList;
+  DiskFileName,
+  fileName,
+  ArchiveFileName : String;
+
+begin
+  Result := False;
+  OutputDir:=includeTrailingPathDelimiter(OutputDir);
+  if DirectoryExists(OutputDir) then
+  begin
+    ZipperObj := TZipper.Create;
+    FileList     := TStringList.create;
+    try
+      ZipperObj.FileName := OutputDir + ExtractFileName(File2Zip) + '.zip';
+      if not FileExists(ZipperObj.FileName) then
+      begin
+        FileList := FindAllFiles(File2Zip);
+        for filecounter := 0 to FileList.Count-1 do
+        begin
+          DiskFileName := FileList.Strings[filecounter];
+          ArchiveFileName:=StringReplace(FileList.Strings[filecounter],File2Zip,'',[rfReplaceall]);
+          ArchiveFileName:=UnicodeString(ArchiveFileName);
+          ArchiveFileName:=UTF8ToCP866(ArchiveFileName);
+          ZipperObj.Entries.AddFileEntry(DiskFileName, ArchiveFileName);
+        end;
+        ZipperObj.ZipAllFiles;
+        Result := True;
+      end;
+    finally
+      ZipperObj.Free;
+      FileList.Free;
+    end;
+  end;
+end; 
 
 // unzip to the output directory or to the zip file directory(if output Directory is not mentioned) while preserving its directory structure.
 function UnzipWithDirStruct(File2Unzip, OutputDir :String): Boolean;
