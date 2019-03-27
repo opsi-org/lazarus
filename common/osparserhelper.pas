5be906a8f5e6785fb791637437c1d62e6982c7f5
d.oertel <d.oertel@uib.de>
2019-03-27 13:55:12 +0100

    4.12.1: osparserhelper: fix at getouterfunction

--- common/osparserhelper.pas
+++ common/osparserhelper.pas
@@ -314,7 +314,9 @@ begin
     if closeBracketsNum > openBracketsNum then
     begin
       // we should cut at the matching close brackets
-      cutpos := NPos(')',s,openBracketsNum);
+      //cutpos := NPos(')',s,openBracketsNum);
+      // give me the pos of the last closing bracket
+      cutpos := NPos(')',s,closeBracketsNum);
       Expression := copy(s,1,cutpos-1);
       Remaining := Remaining + copy(s,cutpos,length(s));;
     end
