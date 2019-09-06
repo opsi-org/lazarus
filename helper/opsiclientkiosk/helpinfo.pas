69162b1a64a68ea44d9be2380f84d22c216258f3
Jan Werner <j.werner@uib.de>
2019-09-06 15:04:14 +0200

    Including help dialog

--- helper/opsiclientkiosk/helpinfo.pas
+++ helper/opsiclientkiosk/helpinfo.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, proginfo,
-  lcltranslator, Buttons;
+  lcltranslator, Buttons, lclintf, ExtCtrls;
 
 type
 
@@ -14,13 +14,39 @@ type
 
   TFormHelpInfo = class(TForm)
     ButtonClose: TButton;
-    ButtonAbout: TButton;
     CheckBoxExpertMode: TCheckBox;
-    MemoInfo: TMemo;
-    procedure ButtonAboutClick(Sender: TObject);
+    ImageLogo: TImage;
+    LabelCopy: TLabel;
+    LabelCopyRight: TLabel;
+    LabelCredits: TLabel;
+    LabelCreditsTo: TLabel;
+    LabelLang: TLabel;
+    LabelLanguage: TLabel;
+    LabelName: TLabel;
+    LabelOpsiWeb: TLabel;
+    LabelUibWeb: TLabel;
+    LabelVers: TLabel;
+    LabelVersion: TLabel;
+    PanelDescriptions: TPanel;
+    PanelInfo: TPanel;
+    PanelInfoContent: TPanel;
+    PanelTop: TPanel;
+    PanelCredits: TPanel;
+    PanelBottom: TPanel;
+    PanelVersion: TPanel;
+    SpeedButtonManual: TSpeedButton;
     procedure ButtonCloseClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure LabelOpsiWebClick(Sender: TObject);
+    procedure LabelOpsiWebMouseEnter(Sender: TObject);
+    procedure LabelOpsiWebMouseLeave(Sender: TObject);
+    procedure LabelUibWebClick(Sender: TObject);
+    procedure LabelUibWebMouseEnter(Sender: TObject);
+    procedure LabelUibWebMouseLeave(Sender: TObject);
+    procedure SpeedButtonManualClick(Sender: TObject);
   private
+    procedure MouseEnterLink(Sender: TObject);
+    procedure MouseLeaveLink(Sender: TObject);
 
   public
 
@@ -29,8 +55,9 @@ type
 var
   FormHelpInfo: TFormHelpInfo;
 
-{resourcestring
-  rsExpertMode = 'Expert mode';}
+//resourcestring
+  //rsExpertMode = 'Expert mode';
+
 
 
 implementation
@@ -42,30 +69,67 @@ implementation
 procedure TFormHelpInfo.FormCreate(Sender: TObject);
 begin
    //SpeedButtonExpertMode.Caption := rsExpertMode;
-   MemoInfo.Lines.Add(
-  'Documentation: www.opsi.org' + LineEnding +
-  '-----------------------------------' + LineEnding +
-  'opsi-client-kiosk' + LineEnding +
-  'Display language: ' + GetDefaultLang + Lineending +
-  'Version: ' + ProgramInfo.Version + Lineending +
-  'CopyRight: uib gmbh (http://uib.de) under AGPLv3' + LineEnding +
-  'http://opsi.org' + Lineending +
-  'Credits to: Lazarus/FPC,synapse,sqllite');
+  LabelVersion.Caption := ProgramInfo.Version;
+  LabelLanguage.Caption := GetDefaultLang;
+  LabelCopyRight.Caption:= 'uib gmbh under AGPLv3';
+  LabelUibWeb.Caption := 'http://uib.de';
+  LabelOpsiWeb.Caption:= 'https://opsi.org';
+  LabelCredits.Caption:= 'Lazarus/FPC, synapse, sqllite';
 end;
 
-
-procedure TFormHelpInfo.ButtonAboutClick(Sender: TObject);
+procedure TFormHelpInfo.LabelOpsiWebClick(Sender: TObject);
 begin
-    ShowMessage(
-  'Documentation: www.opsi.org' + LineEnding +
-  '-----------------------------------' + LineEnding +
-  'opsi-client-kiosk' + LineEnding +
-  'Display language: ' + GetDefaultLang + Lineending +
-  'Version: ' + ProgramInfo.Version + Lineending +
-  'CopyRight: uib gmbh (http://uib.de) under AGPLv3' + LineEnding +
-  'http://opsi.org' + Lineending +
-  'Credits to: Lazarus/FPC,synapse,sqllite');
+  OpenUrl('https://opsi.org');
 end;
+
+procedure TFormHelpInfo.LabelOpsiWebMouseEnter(Sender: TObject);
+begin
+  MouseEnterLink(Sender);
+end;
+
+procedure TFormHelpInfo.LabelOpsiWebMouseLeave(Sender: TObject);
+begin
+  MouseLeaveLink(Sender);
+end;
+
+procedure TFormHelpInfo.LabelUibWebClick(Sender: TObject);
+begin
+  OpenUrl('https://uib.de');
+end;
+
+procedure TFormHelpInfo.LabelUibWebMouseEnter(Sender: TObject);
+begin
+  MouseEnterLink(Sender);
+end;
+
+procedure TFormHelpInfo.LabelUibWebMouseLeave(Sender: TObject);
+begin
+  MouseLeaveLink(Sender);
+end;
+
+procedure TFormHelpInfo.SpeedButtonManualClick(Sender: TObject);
+var
+  languageNotFound:boolean;
+begin
+   if GetDefaultLang = 'de' then
+     OpenUrl('https://download.uib.de/opsi4.1/documentation/html/'+
+       'opsi-manual-v4.1/opsi-manual-v4.1.html#software-on-demand')
+   else OpenUrl('https://download.uib.de/opsi4.1/documentation/html/en/' +
+     'opsi-manual-v4.1/opsi-manual-v4.1.html#software-on-demand');
+end;
+
+procedure TFormHelpInfo.MouseEnterLink(Sender: TObject);
+begin
+  Screen.Cursor := crHandPoint;
+  (Sender as TControl).Font.Style := [fsUnderline];
+end;
+
+procedure TFormHelpInfo.MouseLeaveLink(Sender: TObject);
+begin
+  Screen.Cursor := crDefault;
+  (Sender as TControl).Font.Style := [];
+end;
+
 
 procedure TFormHelpInfo.ButtonCloseClick(Sender: TObject);
 begin
