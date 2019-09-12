ac490f5cc42f34a84ae3419cfa6f5d10fff2c15d
Jan Werner <j.werner@uib.de>
2019-09-12 18:04:43 +0200

    opsi-icon-collector put into git

---
+++ helper/opsi-icon-collector/iconcollector.pas
@@ -0,0 +1,47 @@
+unit IconCollector;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil;
+
+type
+
+  { TIconCollector }
+
+  TIconCollector = class(TObject)
+    private
+      FileNames :TStringList;
+      //function ExtractIcon(OpsiScriptPath:String):String;
+      //function ExtractProductID(OpsiScriptPath:String):String;
+    public
+      procedure ShowFilenames;
+      constructor Create(DepotPath: String);
+      destructor Destroy; override;
+  end;
+
+implementation
+
+{ TIconCollector }
+
+procedure TIconCollector.ShowFilenames;
+begin
+  WriteLn(FileNames.Text);
+end;
+
+constructor TIconCollector.Create(DepotPath: String);
+begin
+  inherited Create;
+  FileNames := FindAllFiles(DepotPath,'setup.opsiscript;setup32.opsiscript');
+end;
+
+destructor TIconCollector.Destroy;
+begin
+  inherited Destroy;
+  FileNames.Free;
+end;
+
+end.
+
