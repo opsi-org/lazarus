086904511bdf1903f0a22d7b1ecf7e1908ca2430
Jan Werner <j.werner@uib.de>
2019-10-22 16:35:38 +0200

    Including copyright notice

--- external_libraries/GZIPUtils/gziputils.pas
+++ external_libraries/GZIPUtils/gziputils.pas
@@ -10,6 +10,20 @@
 //
 //  modified by uib gmbh, author: Jan Werner
 // *****************************************************************************
+// Copyright (c) 2007-2019, Udo Schmal <udo.schmal@t-online.de>
+
+// Permission to use, copy, modify, and/or distribute the software for any purpose
+// with or without fee is hereby granted, provided that the above copyright notice
+// and this permission notice appear in all copies.
+
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+// OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+// DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+// ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// *****************************************************************************
+
 unit GZIPUtils;
 {$ifdef fpc}
   {$mode objfpc}
