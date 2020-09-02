a5e8e420759c5d12f8914409803826d57158bc28
doertel <d.oertel@uib.de>
2020-09-02 21:43:01 +0200

    //* macos: changeback mountpoint to /Volumes/opsi_depot   * macos: change mountpoint back to /var/opsisetupadmin/opsi_depot

--- opsi-script/osconf.pas
+++ opsi-script/osconf.pas
@@ -267,7 +267,8 @@ begin
 {$ENDIF LINUX}
 {$IFDEF DARWIN}
     //depotdrive := '/Network/opsi_depot';
-    depotdrive := '/Volumes/opsi_depot';
+    //depotdrive := '/Volumes/opsi_depot';
+    depotdrive := '/var/opsisetupadmin/opsi_depot';
 (*
     RunCommand('sw_vers -productVersion', outstr);
     if trim(outstr) > '10.14' then
@@ -513,6 +514,10 @@ end;
 
 initialization
   opsiscriptconf := opsiscriptconfinit;
+  {$IFDEF DARWIN}
+  //force_min_loglevel := 8;
+  //debug_prog:= True;
+  {$ENDIF DARWIN}
   initEncoding;
 (*
 {$IFDEF WINDOWS}
