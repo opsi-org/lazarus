1f3cfd1adf876969ce8963932f1f9d9c6da367a4
doertel <d.oertel@uib.de>
2020-09-23 17:22:13 +0200

    Update unit1.lfm and unit1.pas

--- helper/gtk2test/unit1.pas
+++ helper/gtk2test/unit1.pas
@@ -35,12 +35,16 @@ procedure TForm1.FormShow(Sender: TObject);
 begin
   //Sleep(1000);
   //Application.Terminate;
+   writeln('start form / gtk test ...');
    Timer1.Enabled:=true;
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
 begin
+   Timer1.Enabled:=false;
+   writeln('terminating regulary');
    Application.Terminate;
+   halt(0);
 end;
 
 end.
