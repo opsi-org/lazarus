82f2b1584a3e8cd79a0ab4c80c41b20c03ffeee6
Detlef Oertel <d.oertel@uib.de>
2020-11-12 18:23:22 +0100

    opsi-setup-detector: translation fixes

--- opsi-misc/opsisetupdetector/osdcheckentriesdlg.pas
+++ opsi-misc/opsisetupdetector/osdcheckentriesdlg.pas
@@ -16,7 +16,7 @@ type
     BitBtn1: TBitBtn;
     CheckBoxDoNotShowCheckEntries: TCheckBox;
     Label1: TLabel;
-    Memo1: TMemo;
+    Label2: TLabel;
     Panel1: TPanel;
     Panel2: TPanel;
     procedure FormCreate(Sender: TObject);
@@ -32,30 +32,32 @@ var
 
   resourcestring
 
-    rscheckEntriesMsg = 'You have to check every data field if this data are correct or plausible.';
+  //  rscheckEntriesMsg = 'You have to check every data field if this data are correct or plausible.';
 (*
     rscheckEntriesMsg =
-        'The following data are autoamatically detected. ' +
+        'The following data are automatically detected. ' +
         'You have to check every data field if this data are correct or plausible. ' +
         'For some data you may have to install the program once  ' +
         'and than get the needed data from the completed installation.';
         *)
-(*
+
   rscheckEntriesMsg =
-    'The following data are autoamatically detected.' + Lineending +
+    'The following data are automatically detected.' + Lineending +
     'You have to check every data field if this data are correct or plausible.' +  Lineending +
     'For some data you may have to install the program once ' +  Lineending +
     'and than get the needed data from the completed installation.';
-*)
+
 implementation
 
 {$R *.lfm}
+
 
 { TFCheckenties }
 
 procedure TFCheckenties.FormCreate(Sender: TObject);
 begin
-  Memo1.Text:= rscheckEntriesMsg;
+  //Memo1.Text:= rscheckEntriesMsg;
+  label2.Caption:= rscheckEntriesMsg;
 end;
 
 end.
