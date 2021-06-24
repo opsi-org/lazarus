5b9e7d1406cfc0322b287afbc3368e298eb49f5e
Jan Werner <j.werner@uib.de>
2021-06-24 14:44:22 +0200

    New unit ossslpaths.pas - function GetSSLPath, tries to get the correct path to the ssl libs

---
+++ opsi-script/ossslpaths.pas
@@ -0,0 +1,99 @@
+unit osSSLPaths;
+// This code is part of the opsi.org project
+
+// Copyright (c) uib gmbh (www.uib.de)
+// This sourcecode is owned by the uib gmbh, D55118 Mainz, Germany
+// and published under the Terms of the GNU Affero General Public License.
+// Text of the AGPL: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+// author: Jan Werner
+// credits: http://www.opsi.org/credits/
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, osLog;
+
+function GetSSLPath(LibName: string):string;
+(*******************************************
+  GetSSLPath tries to determine the correct path to the ssl library provided by opsi.
+  Gives back the path to the ssl library if the library was found in the filesystem
+  otherwise LibName is returned
+********************************************)
+
+implementation
+
+
+function GetSSLPath(LibName: string): string;
+var
+  i : integer;
+  Path : TStringList;
+  FileList : TStringlist;
+begin
+  Path := TStringList.Create;
+  try
+    Path.Clear;
+    Path.Sorted := False;
+    {$IFDEF WINDOWS}
+      Path.Append(ProgramDirectory + LibName);
+    {$ENDIF WINDOWS}
+    {$IFDEF LINUX}
+      Path.Append(ProgramDirectory + LibName);
+      {$IFDEF OPSISCRIPT}
+        Path.Append('/opt/opsi-script/' + LibName);
+        Path.Append('/opt/opsi-script-nogui/' + LibName);
+        Path.Append('/tmp/opsi-linux-client-agent/files/opsi-script/' + LibName);
+        Path.Append('/tmp/opsi-linux-client-agent/files/opsi-script-nogui/' + LibName);
+        Path.Append('/tmp/opsi-linux-client-agent/files/opsi/opsi-script/64/opsi-script-nogui/' + LibName);
+        Path.Append('/tmp/opsi-linux-client-agent/files/opsi/opsi-script/64/opsi-script/' + LibName);
+        Path.Append('/tmp/opsi-linux-client-agent/files/opsi/opsi-script/32/opsi-script-nogui/' + LibName);
+        Path.Append('/tmp/opsi-linux-client-agent/files/opsi/opsi-script/32/opsi-script/' + LibName);
+      {$ENDIF OPSISCRIPT}
+      Path.Append('/usr/bin/' + LibName);
+    {$ENDIF LINUX}
+    {$IFDEF DARWIN}
+      {$IFDEF APP_BUNDLE}
+        Path.Append(ProgramDirectory + '../Frameworks/' + LibName);
+      {$ELSE}
+        Path.Append(ProgramDirectory + LibName);
+        Path.Append('/usr/local/lib/' + LibName);
+        if (LibName = 'libssl.dylib') then
+        begin
+          FileList := FindAllFiles('/usr/local/lib/','libssl.*.dylib',false);
+        end
+        else if (LibName = 'libcrypto.dylib') then
+        begin
+          FileList := FindAllFiles('/usr/local/lib/','libcrypto.*.dylib',false);
+        end;
+        if FileList.Count > 0 then
+          Path.Append(ExtractFileName(FileList.Strings[FileList.count - 1]));
+        Path.Append(ProgramDirectory + '../Frameworks/' + LibName);
+      {$ENDIF APP_BUNDLE}
+    {$ENDIF DARWIn}
+    Path.Append(LibName);
+    i := 0;
+    //while not FileIsInDirectory(LibName, Path[i]) or (i < (Path.Count)-1) do
+    while not FileExists(Path.Strings[i]) or (i < (Path.Count-1)) do
+    begin
+      LogDatei.log('Library: ' + Path.Strings[i] + ' not found.' ,LLDebug);
+      inc(i);
+    end;
+    if (i < (Path.Count-1)) then
+      LogDatei.log('GetSSLPath: ' + Path.Strings[i], LLNotice)
+    else
+      LogDatei.log('GetSSLPath: ' + Path.Strings[i] + ' Note: No library found in any given path. Set path to libname', LLNotice);
+    Result := Path.Strings[i];
+  finally
+    if Assigned(Path) then
+      FreeAndNil(Path);
+    if Assigned(FileList) then
+      FreeAndNil(FileList);
+  end;
+end;
+
+
+
+
+end.
+
