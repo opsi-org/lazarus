1b47d88938545720004aef421a8ba4756cd01697
Jinene Laajili <j.laajili@uib.de>
2021-07-30 17:37:38 +0200

    Adding function AddFileACL to osfunc

--- opsi-script/osfunc.pas
+++ opsi-script/osfunc.pas
@@ -45,6 +45,7 @@ uses
   //unitImpersonator,
   osfuncwin2,
   osfuncwin3,
+  JwaWindows,
 {$IFDEF WIN32}
   osregistry,
   DSiWin32,
@@ -11606,4 +11607,61 @@ end;
   var ErrorInfo: string): boolean;
   GetFinalPathNameByHandle()
 *)
+
+{$IFDEF WINDOWS}
+{This function modifies Access Rights of a file under Windows.
+When calling this function, use :
+AccessPermissions : GENERIC_ALL
+AccessMode :  SET_ACCESS
+Inheritance : SUB_CONTAINERS_AND_OBJECTS_INHERIT }
+
+function AddFileACL(Filename, TrusteeName: AnsiString; AccessPermissions: DWord;
+                AccessMode: ACCESS_MODE; Inheritance: DWord): boolean; stdcall;
+var
+  ExplicitAccess: EXPLICIT_ACCESS;
+  ExistingDacl: ACL;
+  PExistingDacl: PACL;
+  newACL: PACL;
+  mySD: SECURITY_DESCRIPTOR;
+  pSD : PSECURITY_DESCRIPTOR;
+  errorstr: String;
+  myDWord: DWord = 1;
+begin
+  logdatei.log('-- Entering AddFileACL function -- ', LLinfo);
+  newACL := nil;
+  pSD := @mySD;
+  PExistingDacl := @ExistingDacl;
+  Result := False;
+  myDWord := GetNamedSecurityInfo(pAnsiChar(Filename), SE_FILE_OBJECT,
+                DACL_SECURITY_INFORMATION, nil, nil, @PExistingDacl, nil, pSD);
+  if myDWord = ERROR_SUCCESS then
+  begin
+    logdatei.log('First Success 1/3 : GetNamedSecurityInfo ', LLinfo);
+    BuildExplicitAccessWithName(@ExplicitAccess, pAnsiChar(TrusteeName),
+                                    AccessPermissions, AccessMode, Inheritance);
+    myDWord := SetEntriesInAcl(1, @ExplicitAccess, PExistingDacl, newACL);
+    if myDWord = ERROR_SUCCESS then
+    begin
+      logdatei.log('Second Success 2/3 : SetEntriesInAcl', LLinfo);
+      try
+        myDWord := SetNamedSecurityInfo(pAnsiChar(Filename),
+              SE_FILE_OBJECT, DACL_SECURITY_INFORMATION, nil, nil, newACL, nil);
+        if myDWord = ERROR_SUCCESS then
+        begin
+          logdatei.log('Third Success 3/3 : SetNamedSecurityInfo ', LLinfo);
+          Result := True;
+        end;
+      except
+        on e: Exception do
+        begin
+          errorstr := e.ClassName + ' -- ' + e.Message;
+          writeln(errorstr);
+        end;
+      end;
+    end;
+  end;
+  logdatei.log('-- Finishing AddFileACL function -- ', LLinfo);
+end;
+{$ENDIF WINDOWS}
+
 end.
