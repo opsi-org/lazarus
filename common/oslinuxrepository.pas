32ccdb7cba64deda977cb6afda914d9871657b0f
Anja-M. Schmitz <a.schmitz@uib.de>
2021-08-31 10:35:25 +0200

    'apt-key add - < Release.key' didn't work any more

--- common/oslinuxrepository.pas
+++ common/oslinuxrepository.pas
@@ -193,7 +193,7 @@ begin
       ' ' + 'Release.key', Output);
     // apt-key add is deprecated (last available in Debian 11, Ubuntu 22.04) and needs gnupg
     FRunCommandelevated.Run('apt-get install gnupg2', Output);
-    FRunCommandElevated.Run('apt-key add - < Release.key', Output);
+    FRunCommandElevated.Run('apt-key add Release.key', Output);
     FRunCommandElevated.Run('rm Release.key', Output);
   except
     LogDatei.log('Exception while adding repository.', LLDebug);
