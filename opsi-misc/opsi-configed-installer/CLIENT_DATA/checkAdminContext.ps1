e0d81fadabbea1318691099561e7487d80cada8d
Anja-M. Schmitz <a.schmitz@uib.de>
2021-11-03 15:54:36 +0100

    latest configed (CLIENT_DATA and OPSI) from bonifax in build folder

--- opsi-misc/opsi-configed-installer/CLIENT_DATA/checkAdminContext.ps1
+++
@@ -1,2 +0,0 @@
-$user=[Security.Principal.WindowsIdentity]::GetCurrent();
-return (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) 
