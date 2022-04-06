f60a74d178bbc92dd70a257b5616ee98d60fce48
schmitz <a.schmitz@uib.de>
2022-04-06 12:12:49 +0200

    Suggestion 2: object oriented (longer code but easier to read)

--- common/osparserhelper_getstringexpressionword.pas
+++ common/osparserhelper_getstringexpressionword.pas
@@ -8,21 +8,134 @@ uses
   Classes, SysUtils,
   osparserhelper;
 
+type
+  TStringExpressionWordReader = class(TObject)
+  private
+    FIndex: integer;
+    FInConstantString: boolean;
+    FQuotes: string;
+    FNumberBrackets: integer;
+    FStringToExermine: string;
+    FWordDelimiterSet: TCharSet;
+  public
+    constructor Create(StringToExermine: string; WordDelimiterSet: TCharSet); overload;
+    function IsBeginningOfConstantString: boolean;
+    function IsDelimiter: boolean;
+    function IsValidDelimiter: boolean;
+    procedure CheckEndOfConstantString;
+    procedure GetStringExpressionWordAndRemaining(var StringExpressionWord: string;
+      var Remaining: string);
+    procedure ReadStringExpressionWord(var StringExpressionWord: string;
+      var Remaining: string);
+  end;
 
-function GetStringExpressionWord(const StringToExermine: string; var Remaining: string;
-  const WordDelimiterSet: TCharset): string;
+
+function GetStringExpressionWord(const StringToExermine: string;
+  var Remaining: string; const WordDelimiterSet: TCharset): string;
 
 
 implementation
 
-function GetStringExpressionWord(const StringToExermine: string; var Remaining: string;
-  const WordDelimiterSet: TCharset): string;
+constructor TStringExpressionWordReader.Create(StringToExermine: string;
+  WordDelimiterSet: TCharSet); overload;
+begin
+  inherited Create;
+  FIndex := 1;
+  FInConstantString := False;
+  FQuotes := '';
+  FNumberBrackets := 0;
+  FStringToExermine := StringToExermine;
+  FWordDelimiterSet := WordDelimiterSet;
+end;
+
+function TStringExpressionWordReader.IsBeginningOfConstantString: boolean;
+begin
+  Result := False;
+  if (FStringToExermine[FIndex] in ['"', '''']) then
+  begin
+    FInConstantString := True;
+    FQuotes := FStringToExermine[FIndex];
+    Inc(FIndex);
+    Result := True;
+  end;
+end;
+
+function TStringExpressionWordReader.IsDelimiter: boolean;
+begin
+  Result := True;
+  if not (FStringToExermine[FIndex] in FWordDelimiterSet) then
+  begin
+    // opening brackets (of function calls) have to be closed before the word can end
+    if FStringToExermine[FIndex] = '(' then
+      Inc(FNumberBrackets);
+    if FStringToExermine[FIndex] = ')' then
+      Dec(FNumberBrackets);
+    Inc(FIndex);
+    Result := False;
+  end;
+end;
+
+function TStringExpressionWordReader.IsValidDelimiter: boolean;
+begin
+  Result := False;
+  if (FStringToExermine[FIndex] in FWordDelimiterSet) then
+  begin
+    // if ')' is a delimiter, first all brackets of function calls need to be closed
+    if (FNumberBrackets = 0) then
+      Result := True
+    else
+    begin
+      if FStringToExermine[FIndex] = ')' then
+        Dec(FNumberBrackets);
+      Inc(FIndex);
+    end;
+  end;
+end;
+
+procedure TStringExpressionWordReader.CheckEndOfConstantString;
+begin
+  if (FStringToExermine[FIndex] = FQuotes) then
+  begin
+    FInConstantString := False;
+    FQuotes := '';
+  end;
+end;
+
+procedure TStringExpressionWordReader.GetStringExpressionWordAndRemaining(
+  var StringExpressionWord: string; var Remaining: string);
+begin
+  StringExpressionWord := copy(FStringToExermine, 1, FIndex - 1);
+  Remaining := copy(FStringToExermine, FIndex, length(FStringToExermine) - FIndex + 1);
+  Remaining := CutLeftBlanks(Remaining);
+end;
+
+procedure TStringExpressionWordReader.ReadStringExpressionWord(
+  var StringExpressionWord: string; var Remaining: string);
+begin
+  while (FIndex <= length(FStringToExermine)) do
+  begin
+    if not FInConstantString then
+    begin
+      if not IsBeginningOfConstantString then
+        if IsDelimiter then
+          if IsValidDelimiter then
+            Break; // valid delimiter marks end of word -> stop while-loop
+    end
+    else
+    if FInConstantString then
+    begin
+      CheckEndOfConstantString;
+      Inc(FIndex);
+    end;
+  end;
+  GetStringExpressionWordAndRemaining(StringExpressionWord, Remaining);
+end;
+
+
+function GetStringExpressionWord(const StringToExermine: string;
+  var Remaining: string; const WordDelimiterSet: TCharset): string;
 var
-  i: integer = 0;
-  t: string = '';
-  InConstantString : boolean = False;
-  Quotes: string = '';
-  NumberBrackets: integer = 0;
+  StringExpressionWordReader: TStringExpressionWordReader;
 begin
   if StringToExermine = '' then
   begin
@@ -31,57 +144,12 @@ begin
   end
   else
   begin
-    i := 1;
-    while (i <= length(StringToExermine)) do
-    begin
-      if not InConstantString then
-      begin
-        if (StringToExermine[i] in ['"', '''']) then
-        begin
-          InConstantString := True;
-          Quotes := StringToExermine[i];
-          Inc(i);
-        end
-        else
-        if not (StringToExermine[i] in WordDelimiterSet) then
-        begin
-          if StringToExermine[i] = '(' then
-            Inc(NumberBrackets);
-          if StringToExermine[i] = ')' then
-            Dec(NumberBrackets);
-          Inc(i);
-        end
-        else
-        if (StringToExermine[i] in WordDelimiterSet) then
-        begin
-          if (NumberBrackets = 0) then
-            Break
-          else
-          begin
-            if StringToExermine[i] = ')' then
-              Dec(NumberBrackets);
-            Inc(i);
-          end;
-        end;
-      end
-      else
-      if InConstantString then
-      begin
-        if (StringToExermine[i] = Quotes) then
-        begin
-          InConstantString := False;
-          Quotes := '';
-        end;
-        Inc(i);
-      end;
-    end;
+    StringExpressionWordReader :=
+      TStringExpressionWordReader.Create(StringToExermine, WordDelimiterSet);
+    Result := '';
+    StringExpressionWordReader.ReadStringExpressionWord(Result, Remaining);
+    StringExpressionWordReader.Free;
   end;
-
-  Result := copy(StringToExermine, 1, i - 1);
-  Remaining := copy(StringToExermine, i, length(StringToExermine) - i + 1);
-  Remaining := CutLeftBlanks(Remaining);
 end;
 
-
 end.
-
