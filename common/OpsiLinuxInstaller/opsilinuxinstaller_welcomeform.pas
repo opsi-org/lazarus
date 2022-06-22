373e424767b83005815fea9a63bf9869721f9030
schmitz <a.schmitz@uib.de>
2022-06-22 17:30:06 +0200

    Revert "[common/oli] TOpsiLinuxInstallerWelcomeForm: Initialize log file and data structure in FormCreate"
    This reverts commit c11d0f8da527c5794885f9209c708a81618782cb.

--- common/OpsiLinuxInstaller/opsilinuxinstaller_welcomeform.pas
+++ common/OpsiLinuxInstaller/opsilinuxinstaller_welcomeform.pas
@@ -6,10 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  OpsiLinuxInstaller_BaseForm,
-  LogFileFunctions,
-  opsi_configed_installer_data,
-  osfunclin;
+  OpsiLinuxInstaller_BaseForm;
 
 type
   TOpsiLinuxInstallerWelcomeForm = class(TOpsiLinuxInstallerBaseForm)
@@ -21,43 +18,9 @@ type
     LabelSelLanguage: TLabel;
     CarryOutPanel: TPanel;
     LabelCarryOut: TLabel;
-    procedure FormCreate(Sender: TObject); override;
-  public
-  const
-    LogFileName = 'opsi_configed_installer.log';
   end;
 
 implementation
 
-procedure InitializeDataStructureWithDistribution;
-begin
-  // data structure to store the installer data
-  Data := TConfigedInstallerData.Create;
-  // Following lines take a few seconds and are therefore executed only once at the
-  // beginning of the installer program.
-  {$IFDEF DARWIN}
-  if (RunCommand('/bin/sh', ['-c', 'echo | sysctl kern.ostype'], macDistroName) and
-    RunCommand('/bin/sh', ['-c', 'echo | sysctl kern.osrelease'],
-    macDistroRelease)) then
-  begin
-    Delete(macDistroName, 1, 'kern.ostype: '.Length);
-    Delete(macDistroRelease, 1, 'kern.osrelease: '.Length);
-    Delete(macDistroName, macDistroName.Length, 1);
-    Delete(macDistroRelease, macDistroRelease.Length, 1);
-    //ShowMessage('*' + macDistroName + '*');
-    //ShowMessage('*' + macDistroRelease + '*');
-    Data.DistrInfo.SetNameAndRelease(macDistroName, macDistroRelease);
-  end;
-  {$ELSE}
-  Data.DistrInfo.SetNameAndRelease(getLinuxDistroName, getLinuxDistroRelease);
-  {$ENDIF}
-end;
-
-procedure TOpsiLinuxInstallerWelcomeForm.FormCreate(Sender: TObject);
-begin
-  Inherited FormCreate(Sender);
-  InitializeLogFile(LogFileName);
-  InitializeDataStructureWithDistribution;
-end;
-
 end.
+
