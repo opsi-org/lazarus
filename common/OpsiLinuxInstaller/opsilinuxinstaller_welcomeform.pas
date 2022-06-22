c11d0f8da527c5794885f9209c708a81618782cb
schmitz <a.schmitz@uib.de>
2022-06-22 17:17:15 +0200

    [common/oli] TOpsiLinuxInstallerWelcomeForm: Initialize log file and data structure in FormCreate

--- common/OpsiLinuxInstaller/opsilinuxinstaller_welcomeform.pas
+++ common/OpsiLinuxInstaller/opsilinuxinstaller_welcomeform.pas
@@ -6,7 +6,10 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  OpsiLinuxInstaller_BaseForm;
+  OpsiLinuxInstaller_BaseForm,
+  LogFileFunctions,
+  opsi_configed_installer_data,
+  osfunclin;
 
 type
   TOpsiLinuxInstallerWelcomeForm = class(TOpsiLinuxInstallerBaseForm)
@@ -18,9 +21,43 @@ type
     LabelSelLanguage: TLabel;
     CarryOutPanel: TPanel;
     LabelCarryOut: TLabel;
+    procedure FormCreate(Sender: TObject); override;
+  public
+  const
+    LogFileName = 'opsi_configed_installer.log';
   end;
 
 implementation
 
-end.
+procedure InitializeDataStructureWithDistribution;
+begin
+  // data structure to store the installer data
+  Data := TConfigedInstallerData.Create;
+  // Following lines take a few seconds and are therefore executed only once at the
+  // beginning of the installer program.
+  {$IFDEF DARWIN}
+  if (RunCommand('/bin/sh', ['-c', 'echo | sysctl kern.ostype'], macDistroName) and
+    RunCommand('/bin/sh', ['-c', 'echo | sysctl kern.osrelease'],
+    macDistroRelease)) then
+  begin
+    Delete(macDistroName, 1, 'kern.ostype: '.Length);
+    Delete(macDistroRelease, 1, 'kern.osrelease: '.Length);
+    Delete(macDistroName, macDistroName.Length, 1);
+    Delete(macDistroRelease, macDistroRelease.Length, 1);
+    //ShowMessage('*' + macDistroName + '*');
+    //ShowMessage('*' + macDistroRelease + '*');
+    Data.DistrInfo.SetNameAndRelease(macDistroName, macDistroRelease);
+  end;
+  {$ELSE}
+  Data.DistrInfo.SetNameAndRelease(getLinuxDistroName, getLinuxDistroRelease);
+  {$ENDIF}
+end;
 
+procedure TOpsiLinuxInstallerWelcomeForm.FormCreate(Sender: TObject);
+begin
+  Inherited FormCreate(Sender);
+  InitializeLogFile(LogFileName);
+  InitializeDataStructureWithDistribution;
+end;
+
+end.
