f72964cba2185119815c8483888a4148f8acd0e7
schmitz <a.schmitz@uib.de>
2022-06-22 16:50:52 +0200

    [common/oli] FormAppearanceFunctions: new procedure CenterFormOnForm

--- common/OpsiLinuxInstaller/FormAppearanceFunctions.pas
+++ common/OpsiLinuxInstaller/FormAppearanceFunctions.pas
@@ -8,6 +8,7 @@ uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
 
 procedure CenterFormOnScreen(Form: TForm);
+procedure CenterFormOnForm(TopForm: TForm; BottomForm: TForm);
 // switch from one form to the next (set position of new form and adjust visibilities)
 procedure showForm(newForm: TForm; oldForm: TForm);
 
@@ -17,6 +18,14 @@ procedure CenterFormOnScreen(Form: TForm);
 begin
   Form.Left := Round((Screen.Width - Form.Width) / 2);
   Form.Top := Round((Screen.Height - Form.Height) / 2);
+end;
+
+procedure CenterFormOnForm(TopForm: TForm; BottomForm: TForm);
+begin
+  TopForm.Left := BottomForm.Left + Round(BottomForm.Width / 2) -
+    Round(TopForm.Width / 2);
+  TopForm.Top := BottomForm.Top + Round(BottomForm.Height / 2) -
+    Round(TopForm.Height / 2);
 end;
 
 // switch to the next form (set position of new form and adjust visibilities)
