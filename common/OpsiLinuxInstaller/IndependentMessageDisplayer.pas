3e2e62cc3b24e75cde8d2d9fe84609469ed89b28
schmitz <a.schmitz@uib.de>
2022-09-14 15:36:27 +0200

    [common] [chg] IndependentMessageDisplayer: use a label and Application.ProcessMessages instead of a thread since that is less complicated

--- common/OpsiLinuxInstaller/IndependentMessageDisplayer.pas
+++ common/OpsiLinuxInstaller/IndependentMessageDisplayer.pas
@@ -6,62 +6,53 @@ interface
 
 uses
   {$IFDEF GUI}
-  Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
   {$ENDIF GUI}
   Classes, SysUtils;
 
 type
-  TMessageThread = class(TThread)
-    FMessage: string;
-    procedure DisplayMessageOnForm; virtual; abstract;
-    procedure DisplayMessageDialog; virtual; abstract;
-  end;
 
   TIndependentMessageDisplayer = class(TObject)
-    FThread: TMessageThread;
-    constructor Create(Thread: TMessageThread = nil);
-    procedure DisplayMessage(Message: string; DisplayOnForm: boolean = False);
-    destructor Destroy; override;
+    {$IFDEF GUI}
+    FMessageLabel: TLabel;
+    constructor Create(MessageLabel: TLabel = nil);
+    {$ENDIF GUI}
+    procedure DisplayMessage(Message: string; DisplayOnLabel: boolean = False);
   end;
 
 
 implementation
 
-constructor TIndependentMessageDisplayer.Create(Thread: TMessageThread = nil);
+{$IFDEF GUI}
+constructor TIndependentMessageDisplayer.Create(MessageLabel: TLabel = nil);
 begin
   inherited Create;
-  FThread := Thread;
+  FMessageLabel := MessageLabel;
 end;
+{$ENDIF GUI}
 
 procedure TIndependentMessageDisplayer.DisplayMessage(Message: string;
-  DisplayOnForm: boolean = False);
+  DisplayOnLabel: boolean = False);
 begin
   {$IFDEF GUI}
-  if DisplayOnForm then
+  if DisplayOnLabel then
   begin
-    FThread.FMessage := Message;
-    FThread.Synchronize(@FThread.DisplayMessageOnForm);
+    FMessageLabel.Caption := Message;
+    Application.ProcessMessages;
   end
   else
   begin
-    FThread.FMessage := Message;
-    FThread.Synchronize(@FThread.DisplayMessageDialog);
+    ShowMessage(Message);
   end;
   {$ENDIF GUI}
 
   {$IFDEF NOGUI}
-  // For a generalized procedure we need the DisplayOnForm parameter since there
+  // For a generalized procedure we need the DisplayOnLabel parameter since there
   // are two ways in a gui program to display a message but for console applications
   // the parameter doesn't play any role.
-  if DisplayOnForm or not DisplayOnForm then
+  if DisplayOnLabel or not DisplayOnLabel then
     writeln(Message);
   {$ENDIF NOGUI}
-end;
-
-destructor TIndependentMessageDisplayer.Destroy;
-begin
-  if Assigned(FThread) then FreeAndNil(FThread);
-  inherited Destroy;
 end;
 
 end.
