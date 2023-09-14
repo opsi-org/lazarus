9d892e14cbe038468eca0ac3a3d22f3b73a9d5f9
schmitz <a.schmitz@uib.de>
2023-09-14 17:16:51 +0200

    [common] [chg] OpsiLinuxInstaller: New layout for opsi4.3

--- common/OpsiLinuxInstaller/LayoutFunctions.pas
+++ common/OpsiLinuxInstaller/LayoutFunctions.pas
@@ -7,52 +7,31 @@ interface
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
 
-procedure DrawBigBlueLine(Sender: TForm);
-procedure DrawSmallRedLine(Sender: TForm);
 procedure DecorateForm(Sender: TForm);
 
 implementation
 
-procedure DrawBigBlueLine(Sender: TForm);
+procedure DrawRedLine(Sender: TForm);
 var
-  PanelBigLine: TPanel;
-begin
-  // big decoration line at bottom of form in opsi-blue
-  PanelBigLine := TPanel.Create(Sender);
-  PanelBigLine.Parent := Sender;
-  PanelBigLine.ParentColor := False;
-  PanelBigLine.BevelOuter := bvNone;
-  PanelBigLine.Height := 5;
-  PanelBigLine.Top := Sender.Height - PanelBigLine.Height;
-  PanelBigLine.Left := 0;
-  PanelBigLine.Width := Sender.Width;
-  // Opsi-Blau: #006599
-  PanelBigLine.Color := TColor($00996500);
-  PanelBigLine.BevelColor := TColor($00996500);
-end;
-
-procedure DrawSmallRedLine(Sender: TForm);
-var
-  PanelSmallLine: TPanel;
+  PanelRedLine: TPanel;
 begin
   // thin decoration line above the big one in opsi-red
-  PanelSmallLine := TPanel.Create(Sender);
-  PanelSmallLine.Parent := Sender;
-  PanelSmallLine.ParentColor := False;
-  PanelSmallLine.BevelOuter := bvNone;
-  PanelSmallLine.Height := 3;
-  PanelSmallLine.Top := Sender.Height - 11;
-  PanelSmallLine.Left := 0;
-  PanelSmallLine.Width := Sender.Width;
-  // Opsi-Rot: #B42554
-  PanelSmallLine.Color := TColor($005425B4);
-  PanelSmallLine.BevelColor := TColor($005425B4);
+  PanelRedLine := TPanel.Create(Sender);
+  PanelRedLine.Parent := Sender;
+  PanelRedLine.ParentColor := False;
+  PanelRedLine.BevelOuter := bvNone;
+  PanelRedLine.Height := 8;
+  PanelRedLine.Top := Sender.Height - 10;
+  PanelRedLine.Left := 0;
+  PanelRedLine.Width := Sender.Width;
+  // opsi red: #cb1e58
+  PanelRedLine.Color := TColor($00581ECB);
+  PanelRedLine.BevelColor := TColor($00581ECB);
 end;
 
 procedure DecorateForm(Sender: TForm);
 begin
-  DrawBigBlueLine(Sender);
-  DrawSmallRedLine(Sender);
+  DrawRedLine(Sender);
 end;
 
 end.
