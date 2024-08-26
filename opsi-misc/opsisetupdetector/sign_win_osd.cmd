b5523c4f9e50bc98c8fcc402280dcf609913404f
Detlef Oertel <d.oertel@uib.de>
2024-08-26 12:49:30 +0200

    osd_4.3.3: adapt sign_win_osd.cmd to new dev-tool dir

--- opsi-misc/opsisetupdetector/sign_win_osd.cmd
+++ opsi-misc/opsisetupdetector/sign_win_osd.cmd
@@ -1,3 +1,3 @@
 rem account data from uib keepass / codesign / signserver
 call c:\private\codesignpass.cmd
-opsi-dev-tool.exe -l 7 --codesign-server-url=%CODESIGNURL% --codesign-server-password=%CODESIGNPASS% --signserver-sign opsisetupdetector.exe
+c:\opsi.org\opsi-dev-cli\opsi-dev-tool.exe -l 7 --codesign-server-url=%CODESIGNURL% --codesign-server-password=%CODESIGNPASS% --signserver-sign opsisetupdetector.exe
